2e5dc1cc17ee100de83ee7c011d27e01e4628eb2
MUsman27665 <malik012.um@gmail.com>
2020-02-18 13:17:04 +0000

    first commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 #import birthday
-from birthday import fields
+#from birthday import fields
 from birthday import managers
 from django.contrib.auth.models import User
 class UserProfile(models.Model):
